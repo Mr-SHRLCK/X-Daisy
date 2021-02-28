f5abeb92b9ef2c8dda7bebe7f074e9e903e9e89f
unknown <67815810+unknown-01-hacker@users.noreply.github.com>
2021-02-28 10:03:03 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -101,7 +101,7 @@ HELP_STRINGS = f"""
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 You can donate to the original writer's of the Base code,
-Support them  [sher-lock](t.me/mr_srlock)"""
+Support them  [-----](t.me/The_NOoBHaCkeR)"""
 
 IMPORTED = {}
 MIGRATEABLE = []
